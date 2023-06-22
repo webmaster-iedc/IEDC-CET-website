4c43b4a701b73a510c6a37da3b19536beb2de8a8
MVK2803 <mathewvinukariath@gmail.com>
2023-06-22 19:17:55 +0530

    Added Config Files

---
+++ client/src/Pages/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+function Main()
+{
+    return(
+        <div className="bg-black h-screen w-full flex">
+            <Navbar/>
+        </div>
+    );
+}
+export default Main;
