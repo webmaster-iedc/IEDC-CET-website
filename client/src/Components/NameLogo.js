b2149e26aae97faf0075c2edc58824754fe8e888
john-George510 <meetjohn510@gmail.com>
2023-06-24 17:36:35 +0530

    text bug fixed

--- client/src/Components/NameLogo.js
+++ client/src/Components/NameLogo.js
@@ -4,19 +4,18 @@ function NameLogo() {
   return (
     <div className=" text-white  flex flex-col  w-full  h-fit justify-center items-center">
       <div className="w-full flex text-center flex-col lg:w-1/2 ">
-        <h className="text-center text-4xl tracking-widest text-[#5658dd] font-black">
+        <h className="text-center md:text-5xl text-2xl tracking-widest text-[#5658dd] font-black">
           INNOVATION AND ENTREPRENEURSHIP DEVELOPMENT CENTRE
         </h>
         <br />
-
-        <h1 className="text-center text-xl tracking-widest text-white font-thin">
+        <h className="text-center md:text-xl text-xs  tracking-widest text-white font-thin">
           COLLEGE OF ENGINEERING, TRIVANDRUM
-        </h1>
+        </h>
         <br />
         <img src={BgLogo} alt="" />
         <br />
         <h className="text-center text-4xl text-white font-bold">
-        "INNOVATION DISTINGUISHES BETWEEN A LEADER AND A FOLLOWER"
+          "INNOVATION DISTINGUISHES BETWEEN A LEADER AND A FOLLOWER"
         </h>
         <p>-Steve Jobs</p>
       </div>
