8c32e2b39d82a35dde8ba6373ac97cfe9f2e283f
Abhishek A S <98143098+ABHISHEK-A-S@users.noreply.github.com>
2023-06-29 16:52:28 +0530

    Implemented Changes

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -4,17 +4,29 @@ import NameLogo from "../Components/NameLogo";
 import FeatureTiles from "../Components/FeatureTiles/FeatureTiles";
 import About from "../Components/About";
 
+import UpcomingEvents from "../Components/UpcomingEvents";
+import Announcements from "../Components/Announcements";
 import Achievements from "../Components/Achievements";
-function Main()
-{
-    return(
-        <div className="bg-black min-h-screen w-full flex flex-col">
-            <Navbar/>
-            <NameLogo/>
-            <FeatureTiles/>
-            <About/>
-            <Achievements/>
-        </div>
+
+function Main() {
+    return ( <
+        div className = "bg-black min-h-screen w-full flex flex-col" >
+        <
+        Navbar / >
+        <
+        NameLogo / >
+        <
+        FeatureTiles / >
+        <
+        About / >
+        <
+        UpcomingEvents / >
+        <
+        Announcements / >
+        <
+        Achievements / >
+        <
+        /div>
     );
 }
 export default Main;
