06799aa1b0f8bca674e9639a375346efb65d7ae7
MVK2803 <mathewvinukariath@gmail.com>
2023-06-30 13:00:46 +0530

    Minor UI Fixes

--- client/src/Pages/Innovation.js
+++ client/src/Pages/Innovation.js
@@ -1,25 +1,45 @@
 import React from "react";
-import InnovationNavbar from "../Components/InnovationNavbar";
+import Navbar from "../Components/Navbar";
 import InnovationHome from "../Components/InnovationHome";
 import Background from "../Assets/Images/innovation-bg-2.jpg";
 import InnovationAbout from "../Components/InnovationAbout";
-
+import FAQSection from "../Components/FAQ";
 function Innovation()
 {
-    const background = {
-        border: 'none',
-        background: `url(${Background}), black 50% `,
-        backgroundRepeat: 'no-repeat',
-        backgroundPositionX: 'center',
-        backgroundSize: 'cover',
-      };
+    
+      const  navbarItems=  [
+        { text: "HOME", href: "#" },
+        { text: "ABOUT", href: "#" },
+        { text: "ANNOUNCEMENTS", href: "#" },
+        { text: "HIGHLLIGHTS", href: "#" },
+        { text: "FAQs", href: "#" },
+        { text: "CONTACT", href: "#" }
+      ];
+      const faqData=[
+        {
+          question: 'Question a',
+          answer: 'Answer 1',
+        },
+        {
+          question: 'Question 2',
+          answer: 'Answer 2',
+        },
+        {
+          question: 'Question 3',
+          answer: 'Answer 3',
+        },
+        {
+          question: 'Question 4',
+          answer: 'Answer 4',
+        },
+      ];
     return ( 
-    <div className="flex flex-col">
-      <div className="flex-grow" style={{ ...background,backgroundPositionY:"-755px"}}>
-        <InnovationNavbar />
+    <div className = "bg-black min-h-screen w-full flex flex-col" >
+        <Navbar navbarItems={navbarItems} />
         <InnovationHome />
         <InnovationAbout />
-      </div>
+        <FAQSection faqData={faqData }/>
+      
     </div>
      );
 }
