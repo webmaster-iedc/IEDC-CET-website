40073a1a7b8ac58336ebece487f2535e45a3adf3
john-George510 <meetjohn510@gmail.com>
2023-06-30 10:39:59 +0530

    pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router,Routes } from "react-router-dom";
 import './App.css';
 import Innovation from "./Pages/Innovation";
 import Main from './Pages/Main';
-// import Test from './Test/Test';
-function App() {
-  return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/">
-    //       <Main/>
-    //     </Route>
-    //     <Route exact path="/innovation">
-    //       <Innovation/>
-    //     </Route>
-    //   </Switch>
-    // </Router> 
-      // <Main/>
-    <Innovation/>
-  );
+
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/innovation" element={<Innovation />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -3,30 +3,21 @@ import Navbar from "../Components/Navbar";
 import NameLogo from "../Components/NameLogo";
 import FeatureTiles from "../Components/FeatureTiles/FeatureTiles";
 import About from "../Components/About";
-
 import UpcomingEvents from "../Components/UpcomingEvents";
 import Announcements from "../Components/Announcements";
 import Achievements from "../Components/Achievements";
 
 function Main() {
-    return ( <
-        div className = "bg-black min-h-screen w-full flex flex-col" >
-        <
-        Navbar / >
-        <
-        NameLogo / >
-        <
-        FeatureTiles / >
-        <
-        About / >
-        <
-        UpcomingEvents / >
-        <
-        Announcements / >
-        <
-        Achievements / >
-        <
-        /div>
+    return ( 
+        <div className = "bg-black min-h-screen w-full flex flex-col" >
+            <Navbar />
+            <NameLogo />
+            <FeatureTiles />
+            <About />
+            <UpcomingEvents />
+            <Announcements />
+            <Achievements />
+        </div>
     );
 }
 export default Main;
