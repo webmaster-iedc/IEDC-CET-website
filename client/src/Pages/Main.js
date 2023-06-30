acdecc5683b4a7c1df336d15c1ec02388f0d407c
MVK2803 <mathewvinukariath@gmail.com>
2023-06-30 12:42:09 +0530

    Improved Code Readability

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -12,9 +12,38 @@ import Footer from "../Components/Socials";
 import Ino from "../Components/InoPoints";
 import FAQSection from "../Components/FAQ";
 function Main() {
+    //main bar components to enable code reusability
+   const  navbarItems=  [
+        { text: "IGNITES", href: "#" },
+        { text: "ABOUT", href: "#" },
+        { text: "EVENTS", href: "#" },
+        { text: "ANNOUNCEMENTS", href: "#" },
+        { text: "ACHIEVEMENTS", href: "#" },
+        { text: "INO POINTS", href: "#" },
+        { text: "TEAM", href: "#" },
+        { text: "CONTACt", href: "#" }
+      ];
+    const faqData=[
+        {
+          question: 'Question a',
+          answer: 'Answer 1',
+        },
+        {
+          question: 'Question 2',
+          answer: 'Answer 2',
+        },
+        {
+          question: 'Question 3',
+          answer: 'Answer 3',
+        },
+        {
+          question: 'Question 4',
+          answer: 'Answer 4',
+        },
+      ];
     return ( 
     <div className = "bg-black min-h-screen w-full flex flex-col" >
-        <Navbar />
+        <Navbar navbarItems={ navbarItems}/>
         <NameLogo />
         <FeatureTiles />
         <About />
@@ -23,7 +52,7 @@ function Main() {
         <Announcements />
         <Achievements />
         <Testimonials/>
-        <FAQSection/>
+        <FAQSection faqData={faqData }/>
         <ContactPanel/>
         <Footer/>
     </div>
