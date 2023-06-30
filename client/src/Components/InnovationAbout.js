9fa561f60554e9916dd81101c55b206896e93edc
john-George510 <meetjohn510@gmail.com>
2023-06-30 16:20:47 +0530

    innovation highlights added

--- client/src/Components/InnovationAbout.js
+++ client/src/Components/InnovationAbout.js
@@ -9,7 +9,7 @@ function InnovationAbout(){
 		<div className="flex justify-center">
 			<div className="flex flex-col justify-center gap-12">
 				<div className="mt-40 flex flex-col md:flex-row justify-between px-20 mx-5 gap-10">
-					<div className="flex flex-col justify-center">
+					<div className="flex flex-col justify-center items-center md:items-start">
 						<h className="flex text-white text-5xl font-semibold font-poppins">VISION</h>
 						<hr className="mt-2 border-t-2 w-1/4 mx-auto md:ml-10 border-red-700"/>
 						<p className="flex mt-10 text-gray-500 text-xl font-semibold font-inter">To develop a public outreach among<br/> all the professional colleges,<br/> innovative student community in<br/> Kerala</p>
@@ -19,7 +19,7 @@ function InnovationAbout(){
 					</div>
 				</div>
 				<div className="flex md:flex-row-reverse flex-col  justify-between px-20 mx-5 gap-20">
-					<div className="flex flex-col justify-center">
+					<div className="flex flex-col justify-center items-center md:items-start">
 						<h className="flex text-white text-5xl font-semibold font-poppins">ABOUT</h>
 						<hr className="mt-2 border-t-2 w-1/4 mx-auto md:ml-10 border-red-700"/>
 						<p className="flex justify-center mt-10 text-gray-500 text-xl font-semibold font-inter">Innovation on wheels is an initiative put<br/> forward by IEDC CET that aims to spread<br/> technical awareness & inculcate innovation<br/> among students. It consists of workshops,<br/> competitions, lectures and other activities.</p>
@@ -28,11 +28,11 @@ function InnovationAbout(){
 						<img src={About} alt="" className="objects-contain h-5/6 w-5/6"/>
 					</div>
 				</div>
-				<div className="flex flex-col md:flex-row justify-center gap-20 px-20 mx-20 ml-20">
-					<div className="flex flex-col justify-start">
+				<div className="flex flex-col md:flex-row justify-center gap-20 px-20 mx-20">
+					<div className="flex flex-col justify-start items-center md:items-start">
 						<h className="flex text-white text-5xl font-semibold font-poppins">OBJECTIVES</h>
 						<hr className="mt-2 border-t-2 w-1/4 mx-auto md:ml-10 border-red-700"/>
-						<p className="flex mt-10 text-gray-500 text-xl font-semibold font-inter">To foster entrepreneurship and innovation<br/> among students in Kerala. Will undertake<br/> new projects and look forward to<br/> conducting prospective programmes by<br/> engaging with the institutions directly.<br/> To establish a network of aspiring entrepreneurs throughout Kerala.</p>
+						<p className="flex text-center md:text-start mt-10 text-gray-500 text-xl font-semibold font-inter">To foster entrepreneurship and innovation<br/> among students in Kerala. Will undertake<br/> new projects and look forward to<br/> conducting prospective programmes by<br/> engaging with the institutions directly.<br/> To establish a network of aspiring entrepreneurs throughout Kerala.</p>
 					</div>
 					<div className="flex justify-center items-center">
 						<img src={Objectives} alt="" className="objects-contain h-5/6 w-5/6"/>
---
+++ client/src/Components/InnovationHiglights.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Highlights1 from "../Assets/Images/highlights1.png";
+import Arrow from "../Assets/Images/arrowvector.png";
+
+function InnovationHighlights() {
+  const items = [
+    {
+      image: Highlights1,
+      content: "IEDC CET in collaboration with IEDC VAST conducted a workshop on cyber-security at Vidya Academy of Science and Technology. The session was handled by CSCP tech leads and was a part of the Innovation on Wheels program initiated by IEDC CET",
+      date: "26/11/2022"
+    },
+    {
+      image: Highlights1,
+      content: "IEDC CET in collaboration with IEDC VAST conducted a workshop on cyber-security at Vidya Academy of Science and Technology. The session was handled by CSCP tech leads and was a part of the Innovation on Wheels program initiated by IEDC CET",
+      date: "xx/xx/xxxx"
+    }
+  ];
+
+  const [count, setCount] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+	const [direction,setDirection] = useState(true)
+
+  const increment = () => {
+    if (!isTransitioning) {
+			setDirection(true);
+      setIsTransitioning(true);
+      setCount((count + 1) % items.length);
+      setTimeout(() => {
+        setIsTransitioning(false);
+      }, 500);
+    }
+  };
+	const decrement = () => {
+    if (!isTransitioning) {
+			setDirection(false);
+      setIsTransitioning(true);
+			if (count===0){
+				setCount(items.length-1)
+			}
+			else{
+				setCount((count - 1) % items.length);
+			}
+      setTimeout(() => {
+        setIsTransitioning(false);
+      }, 300);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center mb-5">
+      <h1 className="text-white text-center text-5xl font-semibold font-poppins mt-20">
+        HIGHLIGHTS
+      </h1>
+      <hr className="mt-2 border-t-2 w-36 mx-auto mb-20 border-red-700" />
+      <div className="relative flex">
+				<button className="absolute left-32 top-1/2 scale-x-[-1]" onClick={decrement}>
+					<img src={Arrow} alt="" />
+				</button>
+        <div className={`flex flex-col mx-auto justify-center item-center rounded-2xl border-2 border-[#0D0F10] bg-black w-2/5 h-auto transition-transform duration-300 transform ${isTransitioning ?( direction ? '-translate-x-full opacity-0' : 'translate-x-full opacity-0' ) : 'translate-x-0 opacity-100'}` }>
+          <img src={items[count].image} alt="" />
+          <div className="text-[#9BA9B3] text-center text-base font-inter font-semibold p-5">
+            <p>{items[count].content}</p>
+            <p>Date: {items[count].date}</p>
+          </div>
+        </div>
+        <button className="absolute right-32 top-1/2" onClick={increment}>
+          <img src={Arrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default InnovationHighlights;
