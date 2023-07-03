a6b0d6bdf5d19b2bbf9ba0a6d9870e0c27274177
aharonmathews <aharonmathews04@gmail.com>
2023-07-03 23:02:11 +0530

    Folders Named and rearranged

--- client/src/Components/innovation/InnovationContact.js
+++ client/src/Components/innovation/InnovationContact.js
@@ -5,12 +5,12 @@ import {FiPhone} from 'react-icons/fi'
 
 function InnovationContact( {sectionRef}) {
     return(
-        <div  ref={sectionRef} className="items-center">
+        <div  ref={sectionRef} className="items-center mt-4">
             <div className="bg-[#0D0F10] flex flex-col items-center">
-                <h1 className="text-[#ECEDED] text-center text-5xl mt-20 mb-5 font-semibold font-[Inter]">CONTACT</h1>
+                <h1 className="text-[#ECEDED] text-center text-5xl mt-20 mb-5 font-semibold">CONTACT</h1>
                 <hr style={{ height: '2px' }} className="bg-[#9E130A] border-none w-[77px]"/>
                 <div className="mt-7 mb-20">
-                    <div className="flex gap-24 justify-center m-4">
+                    <div className="flex gap-24 m-4">
                         <div>
                             <AiOutlineMail size={45} className="text-[#dee2e4]" />
                         </div>
@@ -18,7 +18,7 @@ function InnovationContact( {sectionRef}) {
                             ceoiedc@cet.ac.in
                         </div>
                     </div>
-                    <div className="flex gap-24 justify-center m-4">
+                    <div className="flex gap-24 m-4">
                         <div>
                             <FiPhone  size={45} className="text-[#dee2e4]"/>
                         </div>
