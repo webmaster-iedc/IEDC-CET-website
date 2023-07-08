e8e289a89b06f8d44880dc32ceee83b5766b2ff0
Noel George <noelgeorge2004@gmail.com>
2023-07-08 19:55:11 +0530

    small changes

--- client/src/Components/ignites/IgnitesNameLogo.js
+++ client/src/Components/ignites/IgnitesNameLogo.js
@@ -3,13 +3,13 @@ import BgLogo from "../../Assets/Images/NameLogo/bgLogo.png";
 
 function NameLogo() {
   return (
-    <div className="text-white flex flex-col w-full h-fit justify-center items-center">
+    <div className="text-white flex flex-col w-full mt-4 h-fit justify-center items-center">
       <div className="w-full flex text-center flex-col lg:w-1/2">
-        <h className="text-center md:text-8xl text-2xl tracking-widest text-[#5658dd] font-black">
+        <h className="text-center md:text-8xl text-4xl tracking-widest text-[#5658dd] font-black">
         <span className="text-white">ignit</span>ES '22
         </h>
         <br />
-        <h className="text-center font-extrabold md:text-4xl text-xs tracking-widest text-white ">
+        <h className="text-center font-extrabold md:text-4xl text-2xl tracking-widest text-white ">
           IEDC CET
         </h>
         
