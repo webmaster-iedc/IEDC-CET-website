99e0d7db0458b9061e1d0ef54f82cf72830958c9
Ivin <ivinmk2410@hotmail.com>
2023-07-14 21:44:48 +0530

    All changes made during addition of Timeline

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -13,9 +13,10 @@ import Ino from "../Components/mainfiles/InoPoints";
 import FAQSection from "../Components/mainfiles/FAQmain";
 import Faculty from "../Components/mainfiles/Faculty";
 import Team from "../Components/mainfiles/Team";
-import Timeline from "../Components/mainfiles/Timeline";
-
 import {RiArrowDropDownLine} from 'react-icons/ri';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import Timeline from "../Components/mainfiles/Timeline";
 
 
 function Main() {
@@ -69,6 +70,7 @@ function Main() {
         <NameLogo />
         <FeatureTiles />
         <About sectionRef={aboutSectionRef} />
+        <Timeline />
         <Ino sectionRef={inoSectionRef}/>
         <UpcomingEvents sectionRef={upcomingEventsSectionRef} />
         <Announcements sectionRef={announcementsSectionRef} />
