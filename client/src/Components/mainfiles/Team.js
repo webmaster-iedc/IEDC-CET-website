cf1ca8372cd9c1a0ec80ceb4690521e004f075f8
Mathphile2807 <149683187+Mathphile2807@users.noreply.github.com>
2024-06-29 14:33:54 +0530

    updated image,name and role

--- client/src/Components/mainfiles/Team.js
+++ client/src/Components/mainfiles/Team.js
@@ -1,175 +1,94 @@
 import React from "react";
 import Slider from "./Slider2";
-import CEO from "../../Assets/Images/Teams/CEO.png"
-import CFO from "../../Assets/Images/Teams/CFO.png"
-import COO from "../../Assets/Images/Teams/COO.jpeg"
-import CCO from "../../Assets/Images/Teams/CCO.jpg"
-import CTO from "../../Assets/Images/Teams/CTO.jpg"
-import CMO_1 from "../../Assets/Images/Teams/CMO_1.jpg"
-import CMO_2 from "../../Assets/Images/Teams/CMO_2.jpg"
-import CCoO_1 from "../../Assets/Images/Teams/CCoO_.png"
-import CCoO_2 from "../../Assets/Images/Teams/CCoO.JPG"
-import CSMO1 from "../../Assets/Images/Teams/CSMO_1.jpg"
-import CSMO2 from "../../Assets/Images/Teams/CSMO_2.jpeg"
-import PRO from "../../Assets/Images/Teams/PRO.jpg"
-import PRO_2 from "../../Assets/Images/Teams/PRO_2.JPG"
-import PO_1 from "../../Assets/Images/Teams/PO_1.jpg"
-import PO_2 from "../../Assets/Images/Teams/PO_2.JPG"
-import PO_3 from "../../Assets/Images/Teams/PO_3.jpeg"
-import PO_4 from "../../Assets/Images/Teams/PO_4.jpg"
-import CC from "../../Assets/Images/Teams/CC.jpg"
-
-import WEL from "../../Assets/Images/Teams/WEL.JPG"
-import NKL from "../../Assets/Images/Teams/NKL.jpg"
-import WM from "../../Assets/Images/Teams/WM.jpg"
+import SL from "../../Assets/Images/Teams/SL.png"
+import SL2 from "../../Assets/Images/Teams/SL_2.png"
+import CDO from "../../Assets/Images/Teams/CDO.jpeg"
+import CFO from "../../Assets/Images/Teams/CFO.jpg"
+import CMO from "../../Assets/Images/Teams/CMO.jpg"
+import COO2 from "../../Assets/Images/Teams/COO_2.jpg"
+import COO from "../../Assets/Images/Teams/COO.jpg"
+import CTO from "../../Assets/Images/Teams/CTO.png"
+import IFR from "../../Assets/Images/Teams/IFR.JPG"
+import PAL from "../../Assets/Images/Teams/PAL.jpg"
+import WIL from "../../Assets/Images/Teams/WIL.jpeg"
 export default function Team() {
     const team = [{
-        image: CEO,
-        name: "Rizvan M S",
-        role: "Chief Executive Officer",
+        image: SL,
+        name: "Treasa Mariya",
+        role: "Student Lead",
         email: "ceoiedc@cet.ac.in",
         number: "+91 7909290803"
     },
     {
-        image: CFO,
-        name: "Arya S Jayasree",
-        role: "Chief Financial Officer",
+        image: SL2,
+        name: "Usman A Asan",
+        role: "Student Lead",
         email: "cfoiedc@cet.ac.in",
         number: "+91 6235741611",
     }, 
     {
-        image: COO,
-        name: "Christo Stanly", 
-        role: "Chief Operating Officer",
+        image: CFO,
+        name: "Muhammad Alfas", 
+        role: "Chief Finance Officer",
         email: "cooiedc@cet.ac.in",
         number: "+91 9747104733"
     },
     {
-        image: CCO,
-        name: "Akhil Hayash M", 
-        role: "Chief Creative Officer",
+        image: IFR,
+        name: "Sarang T K", 
+        role: "IFR & Research Lead",
         email: "ccoiedc@cet.ac.in",
         number: "+91 7994764846"
     },
     {
-        image: CTO,
-        name: "Yena Sunil Kumar", 
-        role: "Chief Technical Officer",
+        image: CMO,
+        name: "Edwin Emmanuel Roy", 
+        role: "Chief Marketing Officer",
         email: "ctoiedc@cet.ac.in",
         number: "+91 7025247468"
     },
     {
-        image: CMO_1,
-        name: "Sidharth Nair", 
-        role: "Chief Marketing Officer",
+        image: CDO,
+        name: "Afreen Nishad", 
+        role: "Chief Design Officer",
         email: "cmoiedc@cet.ac.in",
         number: "+91 9061354702"
     },
     {
-        image:CMO_2,
-        name: "Joel S Thomas", 
-        role: "Chief Marketing Officer",
+        image:COO,
+        name: "Aswin U M", 
+        role: "Chief Operating Officer",
         email: "cmoiedc@cet.ac.in",
         number: "+91 8597005255"
     },
     {
-        image: CCoO_1,
-        name: "Navaneeth P A", 
-        role: "Chief Community Officer",
+        image: PAL,
+        name: "Niranjana G Nair", 
+        role: "Public Affairs Lead",
         email: "ccoiedc@cet.ac.in",
         number: "+91 7025140214"
     },
     {
-        image:CCoO_2,
-        name: "Sarang T K", 
-        role: "Chief Community Officer",
+        image:WIL,
+        name: "Decika J U", 
+        role: "Woman Innovation Lead",
         email: "ccoiedc@cet.ac.in",
         number: "+91 8129196507"
     },
     {
-        image: CSMO1,
-        name: "Aswin U M", 
-        role: "Chief Social Media Officer",
+        image: CTO,
+        name: "Asna Gafoor", 
+        role: "Chief Technical Officer",
         email: "iedc@cet.ac.in",
         number: "+91 0000000000"
     },
     {
-        image: CSMO2,
-        name: "Melijo Jose", 
-        role: "Chief Social Media Officer",
+        image: COO2,
+        name: "Christo Stanly", 
+        role: "Chief Operating Officer",
         email: "iedc@cet.ac.in",
         number: "+91 8075081397"
-    },
-    {
-        image: PRO,
-        name: "Diliya Mol Sebastian", 
-        role: "Public Relations Officer",
-        email: "proiedc@cet.ac.in",
-        number: "+91 8156911365"
-    },
-    {
-        image: PRO_2,
-        name: "Treasa Mariya", 
-        role: "Public Relations Officer",
-        email: "proiedc@cet.ac.in",
-        number: "+91 9400843439"
-    },
-    {
-        image: PO_1,
-        name: "Alan Aji", 
-        role: "Program Officer",
-        email: "poiedc@cet.ac.in",
-        number: "+91 8289965095"
-    },
-    {
-        image: PO_2,
-        name: "Mohammed Fasil", 
-        role: "Program Officer",
-        email: "poiedc@cet.ac.in",
-        number: "+91 9605953690"
-    },
-    {
-        image: PO_3,
-        name: "Varsha Nandakumar", 
-        role: "Program Officer",
-        email: "poiedc@cet.ac.in",
-        number: "+91 9847882852"
-    },
-    {
-        image:PO_4,
-        name: " Bharath Krishna", 
-        role: "Programme Officer",
-        email: "proiedc@cet.ac.in",
-        number: "+91 8281763544"
-    },
-    {
-        image: CC,
-        name: "Rishi Prasad", 
-        role: "Content Creator",
-        email: "cciedc@cet.ac.in",
-        number: "+91 8281180107"
-    },
-    {
-        image: WEL,
-        name: "Sreerekha", 
-        role: "Women Entrepreneurship Lead",
-        email: "iedc@cet.ac.in",
-        number: "+91 6235687850"
-    },
-    {
-        image: NKL,
-        name: "Rigzin Dolma", 
-        role: "Non Keralite Lead",
-        email: "iedc@cet.ac.in",
-        number: "+91 6005580500"
-    },
-    {
-        image:WM,
-        name: "Nived Narayanan", 
-        role: "Web Master",
-        email: "nivednarayanan67@gmail.com",
-        number: "+91 9447300552"
-    } 
+    }
 ]
 
     return ( 
